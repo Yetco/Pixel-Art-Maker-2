21885c483b31be00f85d07ca5a4219df1669db2e
Ify Nonyelu <Younge119@yahoo.com>
2024-09-28 19:01:13 -0500

    added a button on line 27 in HTML and updated js from line 15-18

---
+++ script.js
@@ -0,0 +1,36 @@
+const sizePicker = document.querySelector(".sizePicker");
+const tableCanvas = document.querySelector(".tableCanvas");
+const colorPicker = document.querySelector(".colorPicker");
+const height = document.querySelector(".inputHeight");
+const width = document.querySelector(".inputWidth");
+
+// When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener("submit", function (event) {
+  // Prevent the default action of the form
+  event.preventDefault();
+  // Call makeGrid() function
+  makeGrid(height.value, width.value);
+});
+
+const clearBtn = document.querySelector(".clearBtn");
+clearBtn.addEventListener("click", function () {
+  tableCanvas.innerHTML = "";
+});
+
+// Function to create the grid
+function makeGrid(height, width) {
+  // Loop through the height and width to create the grid
+  for (let row = 0; row < height; row++) {
+    // Insert a row
+    let rows = tableCanvas.insertRow(0);
+    // Loop through the width to insert cells
+    for (let column = 0; column < width; column++) {
+      // Insert a cell
+      let cell = rows.insertCell(0);
+      // Add a click event listener to the cell
+      cell.addEventListener("click", function () {
+        cell.style.backgroundColor = colorPicker.value;
+      });
+    }
+  }
+}
